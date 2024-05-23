0677332baebd811d00823ba0e45ba85796b852ac
okmike <93779056+mbugatti@users.noreply.github.com>
2024-05-23 17:49:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 from setuptools import setup, find_packages
-import os
 
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 DESCRIPTION = 'Quality data analysis with control charts'
 LONG_DESCRIPTION = 'A package that allows to create charts for statistical process control tasks.'
 
-# Setting up
 setup(
-    name="qda",
+    name='qda',
     version=VERSION,
     author="IC Labs (Matteo Bugatti)",
     author_email="<matteo.bugatti@polimi.it>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/polimi-iclabs/qda',
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'statsmodels', 'scipy', 'matplotlib'],
-    keywords=['python', 'statistics', 'statistical process control', 'quality control'],
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
