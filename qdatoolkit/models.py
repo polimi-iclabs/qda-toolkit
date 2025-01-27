d7a79f141832ff8d0ff58f971ff9a3359085a335
alessandra071 <alessandra.scaccabarozzi@polimi.it>
2025-01-27 10:42:33 +0100

    Update models.py
    Updated Assumptions.independence by adding Bartlett and LBQ test

--- qdatoolkit/models.py
+++ qdatoolkit/models.py
@@ -746,7 +746,7 @@ class Assumptions:
 
         return
 
-    def independence(self, alpha=0.05, plots=True, lags=None):
+    def independence(self, plots=True, lags=None):
         """Test the independence of the data.
 
         Parameters
@@ -754,13 +754,8 @@ class Assumptions:
         data : DataFrame
             The data to test for independence.
         
-        alpha : float
-            Significance level.
-                Default value alpha=0.05
-        
         lags : int 
-            Parameter to indicate the independence test to perform: 
-                If None, Runs test
+            Parameter to indicate the independence test to perform (additionally to Runs test): 
                 If lags=1, Bartlett test
                 If lags>1, LBQ test
 
@@ -777,48 +772,27 @@ class Assumptions:
             sgt.plot_pacf(self.data, lags = int(len(self.data)/3), zero=False, ax=ax[1], method = 'ywm')
             plt.show()
 
-        if lags == None:
-            # Runs test 
-            _, pval_runs = runstest_1samp(self.data, correction=False)
-            print('Runs test p-value = {:.3f}'.format(pval_runs))
+
+        # Runs test (default)
+        _, pval_runs = runstest_1samp(self.data, correction=False)
+        print('Runs test p-value = {:.3f}'.format(pval_runs))
 
         if lags is not None:
 
             if lags == 1:
                 #Bartlett's test at lag 1
                 [acf_values, lbq, _] = acf(self.data, nlags = int(np.sqrt(len(self.data))), qstat=True, fft = False)
-
                 rk = acf_values[lags]
-                z_alpha2 = stats.norm.ppf(1-alpha/2)
-                print('\nBartlett test at lag 1:\nTest statistic rk = %f' % rk)
-                print('Rejection region starts at %f' % (z_alpha2/np.sqrt(len(self.data))))
-
                 p_value = 2 * (1 - stats.norm.cdf(abs(rk) * np.sqrt(len(self.data))))
-                print('p-value = %f' % p_value)
-
-                if abs(rk)>z_alpha2/np.sqrt(len(self.data)):
-                    print('The null hypothesis is rejected')
-                else: print('The null hypothesis is accepted')
+                print('Bartlett test (at first lag) p-value = %f' % p_value)
 
             if lags > 1:
 
                 # LBQ test:
                 [acf_values, lbq, _] = acf(self.data, nlags = int(np.sqrt(len(self.data))), qstat=True, fft = False)
                 Q0_LBQ = lbq[lags-1]
-                print('LBQ test:\nQ0_LBQ = %f' % Q0_LBQ)
 
-                #Rejection region for chi square distribution 
-                dof = lags
-                chi2_alfa= stats.chi2.ppf(1-alpha,dof)
-                print('Rejection region starts at %f' % chi2_alfa)
-
-                if Q0_LBQ>chi2_alfa:        
-                    print('The null hypothesis is rejected')                
-                else: 
-                    print('The null hypothesis is accepted')
-
-                # Compute the p-value for the LBQ test
                 pval = 1 - stats.chi2.cdf(Q0_LBQ, lags)
-                print('p-value = %f' % pval)
+                print('LBQ test (at lag %i) p-value = %f' % (lags, pval))
 
         return
